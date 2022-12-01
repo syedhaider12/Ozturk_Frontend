38bf768a1df5cf5ed4946891e44c0304d2323dde
haider <hsyed5649@gmail.com>
2022-12-01 22:52:06 +0500

    add style

--- src/components/touch.js
+++ src/components/touch.js
@@ -13,23 +13,34 @@ function Cont() {
        
         Contact us
       </h1>
-      <div className="grid sm:grid-cols-3 gap-4 grid-row" data-aos="fade-right" 
+      <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 grid-row sm:my-5" data-aos="fade-right" 
         data-aos-duration="1500">
         <div className="flex flex-col items-center justify-between my-3 sm:my-5">
-          <div className="bg-[#031e4a] h-auto rounded-full p-5">
+          <div className="bg-[#031e4a] h-auto rounded-full p-3">
             <PhoneIcon className="w-10 h-10 text-white " />
           </div>
-          <h1 className="text-[#031e4a]">+90 552811230</h1>
+          <h1 className="text-[#031e4a]">+90 5528112230</h1>
         </div>
-        <div className="flex flex-col items-center justify-between my-3 sm:my-5 ">
-          <div className="bg-[#031e4a]  h-auto rounded-full p-5">
+        <div className="flex flex-col items-center justify-between my-3 ">
+          <div className="bg-[#031e4a]  h-auto rounded-full p-3">
             <MailIcon className="w-10 h-10 text-white bg-[#031e4a] " />
           </div>
-          <h1 className="text-[#031e4a]">ozturkrealtors@gmail.com</h1>
+          <div>
+          <h1 className="text-[#031e4a] ">ozturkrealtors@gmail.com</h1>
+          <h1 className="text-[#031e4a] ">Faisal.city.estate@gmail.com</h1>
+          </div>
         </div>
-
         <div className="flex flex-col items-center justify-between my-3">
-          <div className="bg-[#031e4a] h-auto rounded-full p-5">
+          <div className="bg-[#031e4a] h-auto rounded-full p-3">
+            <HomeIcon className="w-10 h-10 text-white " />
+          </div>
+          <h1 className="text-[#031e4a] text-center ">
+          Head Office <br></br>
+          üçevler mah . 911 sk no 4 BH <br></br> Esenyurt/ Istanbul
+          </h1>
+        </div>
+        <div className="flex flex-col items-center justify-between my-3">
+          <div className="bg-[#031e4a] h-auto rounded-full p-3">
             <HomeIcon className="w-10 h-10 text-white " />
           </div>
           <h1 className="text-[#031e4a] ">
@@ -37,6 +48,7 @@ function Cont() {
             Istanbul
           </h1>
         </div>
+      
       </div>
     </>
   );
